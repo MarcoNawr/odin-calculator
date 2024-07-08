c896d702ae9823b1298392cf819e27aef012c948
Marco <166357601+MarcoNawr@users.noreply.github.com>
2024-07-08 20:15:44 +0200

    create: basic  funktion empty shells

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@
 let firstNumber = 0;
 let operator = "";
 let secondNumber = 0;
+let result = 0;
 
 
 /* #######################################################################
@@ -119,4 +120,41 @@ function clearAll(){
     secondNumber = 0;
     displayCurrentCalculation.textContent = "";
     displayCurrentNumber.textContent = "0";
+}
+
+function add(a,b){
+    return a+b;
+}
+
+function subtract(a,b){
+    return a-b;
+}
+
+function multiply(a,b){
+    return a*b
+}
+
+function divide(a,b){
+    return a/b;
+}
+
+function operate(a,b, operator){
+    result = 0;
+    switch (operator) {
+        case "+":
+            result = add(a,b);
+            break;
+        case "-":
+            result = subtract(a,b);
+            break;
+        case "*":
+            result = multiply(a,b);
+            break;
+        case "/":
+            result = divide(a,b);
+            break;
+        default:
+            alert("Something wrong with the operator!");
+      }
+      return result;
 }
